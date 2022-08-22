4ba93c4145211c74e080816abcc7d1d885424208
Balaji-Nandure <balajirn.2016@gmail.com>
2022-08-23 02:09:02 +0530

    Highscore functionality adde

--- script.js
+++ script.js
@@ -7,10 +7,9 @@
 // document.querySelector(`.score`).textContent = 20;
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(".guess").value);
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector(`.number`).textContent = secretNumber;
-
+let highScore = 0;
 document.querySelector(`.check`).addEventListener(`click`, function () {
 	const guess = Number(document.querySelector(`.guess`).value);
 	console.log(typeof guess, guess);
@@ -19,6 +18,14 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 		document.querySelector(".message").textContent = `⛔ No Number`;
 	} else if (guess === secretNumber) {
 		document.querySelector(`.message`).textContent = `🎉 Correct Number`;
+
+		document.querySelector(`.number`).textContent = secretNumber;
+
+		document.querySelector("body").style.backgroundColor = "#60b347";
+		if (score > highScore) {
+			highScore = score;
+			document.querySelector(".highscore").textContent = highScore;
+		}
 	} else if (guess > secretNumber) {
 		if (score > 1) {
 			document.querySelector(`.message`).textContent = `📈 Too High!`;
@@ -38,4 +45,14 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 			document.querySelector(`.score`).textContent = 0;
 		}
 	}
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+	score = 20;
+	secretNumber = Math.trunc(Math.random() * 20 + 1);
+	document.querySelector(`.message`).textContent = `Start guessing...`;
+	document.querySelector(`.score`).textContent = score;
+	document.querySelector(`.number`).textContent = "?";
+	document.querySelector(".guess").value = "";
+	document.querySelector("body").style.backgroundColor = "#222";
 });
