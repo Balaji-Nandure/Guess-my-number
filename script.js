0a819ea1cf00e19bd8b9b950110f0dabf4eed42f
Balaji-Nandure <balajirn.2016@gmail.com>
2022-08-23 01:23:42 +0530

    Add functionality to the game

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 "use strict";
 console.log(document.querySelector(`.message`).textContent);
+document.querySelector(`.message`).textContent = `🎉 Correct Number`;
+
+document.querySelector(`.number`).textContent = 13;
